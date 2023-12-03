9b7021a76e4bc75e49b23820ebf3ec6c8d611033
dzerrm <alexonnn2003@gmail.com>
2023-12-03 05:29:04 +0200

    all the content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from "react";
+import github from "../img/github-logo.png";
+import telegram from "../img/telegram-logo.png"
 
 export default function Footer(){
     return(
         <div className="card--footer">
             <ul className="footer--logos">
-                <li>1</li>
-                <li>2</li>
-
+                <li className="footer--link"> <a href="https://github.com/dzerrm"> <img src={github} alt="github profile link"/> </a> </li>
+                <li className="footer--link"> <a href="https://t.me/dzerrm"> <img src={telegram} alt="telegram profile link"/> </a> </li>
             </ul>
         </div>
     )
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,9 +6,28 @@ export default function MainContent(){
             <h1 className="card--name">Alexander Novikov</h1>
             <p className="card--prof">Junior Front-end Developer</p>
             <p className="card--small-desc">Trying to do things</p>
+
             <div>
-                <button>Email</button>
-                <button>Telegram</button>
+                <a href="mailto:novikov.alexander55@gmail.com"><button>Email</button></a>
+                <a href="https://t.me/dzerrm"><button>Telegram</button></a>
+            </div>
+
+            <div className="content--alltext">
+
+                <p className="content--section-name">About</p>
+                <p className="content--text">Starting programmer without work expirience that is 
+                making some projects and searching for job</p>
+
+                <p className="content--section-name">Interests</p>
+                <p className="content--text">
+                    <ul>
+                        <li>Game design</li>
+                        <li>Roller skating</li>
+                        <li>Tabletop games</li>
+                        <li>Psychology</li>
+                    </ul>
+                </p>
+
             </div>
         </div>
     )
